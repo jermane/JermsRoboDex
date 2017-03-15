b8684cebcee5ef2d38a2034c98801af82b092518
Jermain Joseph <jermjoseph@gmail.com>
2017-03-15 14:07:36 -0400

    keep on trucking

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
+import 'tachyons';
 
 ReactDOM.render(
-  <App />,
+  <div>
+    <img src="//robohash.org/test?size=200x200"/>
+    <div>Jerome Freeman</div>
+    <div>jfreeman@gmail.com </div>
+  </div>,
   document.getElementById('root')
 );
